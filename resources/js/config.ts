8d4a0ae069ef278434c4e45a39d77249b844fa6d
GuillaumeLortet <lortet.guillaume@gmail.com>
2025-03-26 11:00:40 +0100

    chore: update ws uri

--- resources/js/config.ts
+++ resources/js/config.ts
@@ -1,7 +1,7 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 export const config = {
-    wsUrl: isDevelopment ? 'ws://localhost:2567' : 'wss://gamdle-royale.fr',
+    wsUrl: 'ws://localhost:2567',
     apiUrl: isDevelopment ? 'https://gamdle-royale.test' : 'https://gamdle-royale.fr',
     roomUrl: isDevelopment ? 'https://gamdle-royale.test' : 'https://gamdle-royale.fr'
 }; 
