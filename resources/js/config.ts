f6f2106de5a6123644ea1c1039de3a1766cfb5d8
root <root@srv665868.hstgr.cloud>
2025-03-26 12:40:46 +0000

    fix: server config

--- resources/js/config.ts
+++ resources/js/config.ts
@@ -1,7 +1,7 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 export const config = {
-    wsUrl: 'ws://localhost:2567',
+    wsUrl: isDevelopment ? 'ws://localhost:2567' : 'wss://server.gamdle-royale.fr',
     apiUrl: isDevelopment ? 'https://gamdle-royale.test' : 'https://gamdle-royale.fr',
     roomUrl: isDevelopment ? 'https://gamdle-royale.test' : 'https://gamdle-royale.fr'
 }; 
